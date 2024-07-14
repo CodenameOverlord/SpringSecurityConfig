a8599e9c5e924eddf7a0e9899ebcb891bf52e51a
CodenameOverlord <utsavkumardatta@gmail.com>
2024-07-15 01:01:12 +0530

    Role based authorization 1.@PreAuthorize("hasRole('ROLE_NAME')") will allow those user with Role to enter the system. 2. @EnableMethodSecurity  in the security config class to allow this to happen.
     w/o @EnableMethodSecurity, the @preAuthorize("hasRole('ROLE_NAME')") wont work

--- src/main/java/com/example/security/controllers/GreetingController.java
+++ src/main/java/com/example/security/controllers/GreetingController.java
@@ -1,5 +1,6 @@
 package com.example.security.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,20 @@ public class GreetingController {
     @GetMapping("/loginPage")
     public String login(){
         return "Welcome to basic webpage";
+    }
+
+    //preAuthorize authorizes on the basis of role
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/userPage")
+    public String userLogin(){
+        return "This page is accessible to role USER";
+    }
+
+    //preAuthorize authorizes on the basis of role
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/adminPage")
+    public String adminLogin(){
+        return "This page is accessible to role admin";
     }
 
 }

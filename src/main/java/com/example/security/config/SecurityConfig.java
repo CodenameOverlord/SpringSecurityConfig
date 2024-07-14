a7136ca288da21df4e443083970b63ba7d830558
CodenameOverlord <utsavkumardatta@gmail.com>
2024-07-14 23:28:53 +0530

    adding in InMemoryAuthentication without password encoding for getting access to the system, this is for testing purpose only, not production ready

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -4,6 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,7 +20,26 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
 //        http.formLogin(withDefaults());
+        http.sessionManagement(session->{
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+
+    // our functionality here would use inmemory userdetails to fetch and retrieve the data
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user =  User.withUsername("user1")
+                .password("{noop}user123")
+                .roles("USER")
+                .build();
+
+        UserDetails admin =  User.withUsername("admin")
+                .password("{noop}admin123")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }

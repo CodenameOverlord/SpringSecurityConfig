8048503f2e26992b248b6002890f07187a807987
CodenameOverlord <utsavkumardatta@gmail.com>
2024-07-15 22:36:02 +0530

    Creating DataSource: 1. we will create a datasource using inmemoryH2database 2. JDBCUserDetailsManager implements UserDetailsManager which extends UserDetailsService. Now it has a constructor where we can pass in dataSource. (Datasource is created from Bean present in application.property) 3. Failed to instantiate [org.springframework.security.core.userdetails.UserDetailsService]: Factory method 'userDetailsService' threw exception with message: PreparedStatementCallback; bad SQL grammar [insert into users (username, password, enabled) values (?,?,?)]
    since we don't have a schema right now we will be getting this error

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,7 +13,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -20,6 +25,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+
+    @Autowired
+    DataSource dataSource;
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) ->
@@ -51,6 +60,12 @@ public class SecurityConfig {
                 .password("{noop}admin123")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(admin, user);
+
+        UserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(admin);
+        jdbcUserDetailsManager.createUser(user);
+//        return new InMemoryUserDetailsManager(admin, user);
+
+        return jdbcUserDetailsManager;
     }
 }
